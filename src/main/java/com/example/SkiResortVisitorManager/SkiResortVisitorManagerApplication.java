34e3bf28f9dae2fe7abbcd2e5a3c3941788fc1c1
bukaman5 <131355554+bukaman5@users.noreply.github.com>
2023-04-20 18:51:43 +0300

    Initializr

---
+++ src/main/java/com/example/SkiResortVisitorManager/SkiResortVisitorManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkiResortVisitorManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiResortVisitorManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiResortVisitorManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SkiResortVisitorManager/SkiResortVisitorManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkiResortVisitorManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiResortVisitorManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
