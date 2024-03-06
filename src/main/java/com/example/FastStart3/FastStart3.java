b89737b76cb9b5485b1c8c4c5e851b9cf65afac7
bukaman5 <bukman5@gmail.com>
2024-03-06 16:00:20 +0300

    develop a microservice with a RestController containing methods that process HTTP GET, POST, PUT, DELETE requests.

--- src/main/java/com/example/FastStart3/FastStart3.java
+++ src/main/java/com/example/FastStart3/FastStart3.java
@@ -1,13 +1,13 @@
-package com.example.SkiResortVisitorManager;
+package com.example.FastStart3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkiResortVisitorManagerApplication {
+public class FastStart3 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkiResortVisitorManagerApplication.class, args);
+		SpringApplication.run(FastStart3.class, args);
 	}
 
 }
--- src/test/java/com/example/FastStart3/fastStart3Tests.java
+++ src/test/java/com/example/FastStart3/fastStart3Tests.java
@@ -1,10 +1,10 @@
-package com.example.SkiResortVisitorManager;
+package com.example.FastStart3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SkiResortVisitorManagerApplicationTests {
+class FastStart3Tests {
 
 	@Test
 	void contextLoads() {
